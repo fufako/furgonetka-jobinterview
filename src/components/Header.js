89f079e70a242a78e24d42ccacc6e2bf7573fdef
fufako <patryko.mikiciuk@gmail.com>
2022-10-25 11:57:44 +0200

    Create routes and setup header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,42 @@ import Send from "./Send"
 
 function Header(props) {
   const { items, addToCart, handleTotal, removeFromCart } = props
-  return
+  return (
+    <>
+      <div className="header">
+        <Link to="FunnySocks" className="panel">
+          <h1>
+            {" "}
+            <span className="orange">Funny </span> Socks Shop
+          </h1>
+        </Link>
+
+        <div className="nav">
+          <Link className="nav-link" to="furgonetka">
+            Home{" "}
+          </Link>
+          <Link className="nav-link" to="furgonetka/do-wyslania">
+            Do wysłania
+          </Link>
+          <Link className="nav-link" to="furgonetka/saldo">
+            Saldo
+          </Link>
+        </div>
+      </div>
+
+      <Routes>
+        <Route path="/furgonetka/" element={<Panel />} />
+        <Route
+          path="furgonetka/do-wyslania"
+          element={<Send addToCart={addToCart} handleTotal={handleTotal} />}
+        />
+
+        <Route
+          path="furgonetka/saldo"
+          element={<Saldo items={items} removeFromCart={removeFromCart} />}
+        />
+      </Routes>
+    </>
+  )
 }
 export default Header
