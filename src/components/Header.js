7f0091e9f02ccf52a94afca397711b65504867ed
fufako <patryko.mikiciuk@gmail.com>
2022-10-25 13:59:37 +0200

    Update styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,8 @@ import Panel from "./Panel"
 import Saldo from "./Saldo"
 import Send from "./Send"
 import Logo from "../images/furgonetka.png"
+import PanelIcon from "../images/panel_icon.png"
+import SendIcon from "../images/send_icon.png"
 function Header(props) {
   const { items, addToCart, handleTotal, removeFromCart } = props
   return (
@@ -11,22 +13,38 @@ function Header(props) {
         <Link to="furgonetka">
           <img src={Logo} alt="logo-furgonetka" className="header__logo" />
         </Link>
-        <Link to="furgonetka/saldo" style={{ "text-decoration": "none" }}>
+        <Link to="furgonetka/saldo" style={{ textDecoration: "none" }}>
           <div className="header__saldo">Saldo: 0,00 zł</div>
         </Link>
       </header>
 
       <nav className="navigation">
-        <Link to="furgonetka">
-          <div className="navigation__panel navigation__panel--active">
-            Panel
-          </div>
-        </Link>
-        <Link to="furgonetka/do-wyslania">
-          <div className="navigation__do-wyslania navigation__do-wyslania--active">
-            Do wysłania
-          </div>
-        </Link>
+        <div className="navigation__tabs-container">
+          <Link to="furgonetka" style={{ textDecoration: "none" }}>
+            <div className="navigation__panel navigation__panel--active">
+              <img
+                src={PanelIcon}
+                alt="panel-icon"
+                className="navigation__icon"
+              />
+              <p className="navigation__text">Panel</p>
+            </div>
+          </Link>
+          <Link to="furgonetka/do-wyslania" style={{ textDecoration: "none" }}>
+            <div className="navigation__do-wyslania navigation__do-wyslania--active">
+              <img
+                src={SendIcon}
+                alt="send-icon"
+                className="navigation__icon"
+              />
+              <p className="navigation__text">Do wysłania</p>
+            </div>
+          </Link>
+        </div>
+        <div className="navigation__underline-container">
+          <div className="navigation__upper-underline"></div>
+          <div className="navigation__lower-underline"></div>
+        </div>
       </nav>
 
       <Routes>
--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,7 +1,21 @@
 const Panel = () => {
   return (
     <>
-      <div>Panel</div>
+      <div className="dashboard">
+        <div className="dashboard__main-container">
+          <section className="dashboard__promotion">
+            <h4>Jak wysyłać jeszcze taniej?</h4>
+            <div>
+              Stefan prowadzi mały sklep internetowy z modelami statków do
+              sklejania. Dziennie wysyła od 2 do 4 paczek o wadze około 5 kg. Z
+              uwagi na to, że wszystko w ostatnim czasie drożeje, Stefan
+              postanowił przeliczyć, ile kosztuje go wysyłanie paczek obecnie i
+              ile może zaoszczędzić wybierając innego kuriera.
+            </div>
+          </section>
+        </div>
+        <div className="dashboard__side-container"></div>
+      </div>
     </>
   )
 }
